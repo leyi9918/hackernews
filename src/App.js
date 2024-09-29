6fe2a9b6d8cb9b584fd9b501870be5f936e6ac14
leyi9918 <chenleyi2001@gmail.com>
2024-09-29 07:31:23 +0000

    240929 React状态提升
    1. 搜索框输入关键字，页面下的list列表只显示含有关键字的标题

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
 import "./App.css";
 import { useState } from "react";
 
-const stories = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
 
 const List = (props) => {
   return (
@@ -39,43 +21,60 @@ const List = (props) => {
   );
 };
 
-// search如何向上传递数据
-/**
- * search状态
- * 回调函数
- * 返回Jsx(label, input, p, strong)
- */
+
+
+
 const Search = props =>{
-  const [searchTerm, setSearchTerm] = useState("");
-  const handleChange = (e) => {
-    setSearchTerm(e.target.value);
-    // 通过props传递了一个函数过来，onSearch函数，将这个事件通过函数参数进行传递
-    props.onSearch(e);
-  };
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange} />
-
-      <p>
-        searching <strong>{searchTerm}</strong>
-      </p>
+      <input id="search" type="text" onChange={props.onSearch} />
     </div>
   )
 }
+
+
+
+
 /**
  * 显示search,list组件
  */
 const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  const [searchTerm, setSearchTerm] = useState("");
+  
   const handleSearch = event =>{
-    console.log(event.target.value);
+    setSearchTerm(event.target.value);
   }
+
+  const filteredStories = stories.filter(story =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  
   return (
     <div>
       <h1> Hacker News </h1>
       <Search onSearch={handleSearch}/>
       <hr />
-      <List list={stories} />
+      <List list={filteredStories} />
     </div>
   );
 };

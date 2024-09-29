2c59f74732b10f68839d0607d71e7df890f9d337
leyi9918 <chenleyi2001@gmail.com>
2024-09-29 06:25:07 +0000

    240929 JSX回调处理函数 * Search组件分离 * Search组件通过回调函数向App组件传递值

--- src/App.js
+++ src/App.js
@@ -38,20 +38,42 @@ const List = (props) => {
     </div>
   );
 };
-const App = () => {
+
+// search如何向上传递数据
+/**
+ * search状态
+ * 回调函数
+ * 返回Jsx(label, input, p, strong)
+ */
+const Search = props =>{
   const [searchTerm, setSearchTerm] = useState("");
   const handleChange = (e) => {
     setSearchTerm(e.target.value);
+    // 通过props传递了一个函数过来，onSearch函数，将这个事件通过函数参数进行传递
+    props.onSearch(e);
   };
   return (
     <div>
-      <h1> Hacker News </h1>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" onChange={handleChange} />
 
       <p>
         searching <strong>{searchTerm}</strong>
       </p>
+    </div>
+  )
+}
+/**
+ * 显示search,list组件
+ */
+const App = () => {
+  const handleSearch = event =>{
+    console.log(event.target.value);
+  }
+  return (
+    <div>
+      <h1> Hacker News </h1>
+      <Search onSearch={handleSearch}/>
       <hr />
       <List list={stories} />
     </div>
